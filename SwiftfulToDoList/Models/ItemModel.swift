e21320f8809f37278d797c12c397b9d08153be69
Philip Keller <philipjoelkeller@gmail.com>
2023-03-14 23:50:11 -0700

    MVVM ToDo App - Swiftful Thinking 3/8

---
+++ SwiftfulToDoList/Models/ItemModel.swift
@@ -0,0 +1,27 @@
+//
+//  ItemModel.swift
+//  SwiftfulToDoList
+//
+//  Created by Philip Keller on 3/14/23.
+//
+
+import Foundation
+
+//Immutable Struct
+
+struct ItemModel: Identifiable {
+    let id: String
+    let title: String
+    let isCompleted: Bool
+    
+    init(id: String = UUID().uuidString, title: String, isCompleted: Bool) {
+        self.id = id
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+    
+    func updateCompletion() -> ItemModel {
+        return ItemModel(id: id, title: title, isCompleted: !isCompleted)
+    }
+    
+}
